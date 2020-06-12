0f65b09b4dd2d4d2f0a2b486fc6d1749ea6117f6
CoolMLGPlayer <66383446+CoolMLGPlayer@users.noreply.github.com>
2020-06-12 13:20:47 +0100

    Create UploadScript.lua

---
+++ functions/UploadScript.lua
@@ -0,0 +1,18 @@
+
+return function(Content, Developer_Key)
+if not HttpEnabled() then
+return "HTTP Requests are not Enabled!"
+end
+local HttpService = game:GetService("HttpService")
+local worked,link = pcall(function()
+local tocheck = HttpService:PostAsync('http://pastebin.com/api/api_post.php','api_dev_key='..Developer_Key..'&api_option=paste&api_paste_code=' .. HttpService:UrlEncode(Content).."&api_paste_private=1",2)
+if not string.find(tocheck,"pastebin") then
+error("Something errored!")
+end
+end)
+if not worked then
+return "Failed to upload."
+end
+local toreturn = "https://pastebin.com/raw/" .. string.sub(link, 22)
+return toreturn
+end
